b9b709072e4e6efe4aa9c2dc98f5db92db9979b9
ניב באר < >
2018-07-22 13:30:59 +0300

    meta data unacking

--- modules/smush.py
+++ modules/smush.py
@@ -2,19 +2,23 @@ import struct
 import io
 from codex import encode, decode
 
+meta_struct = ('codec', 'xoff', 'yoff', 'width', 'height', 'zero1', 'zero2')
+
 def mktag(tag, data):
     return tag + struct.pack('>I', len(data)) + data
 
 def mkobj(width, height, data, codec, force=None):
     xoff = 0
     yoff = 0
-    res = struct.pack('<H', codec)
-    res += struct.pack('<H', xoff)
-    res += struct.pack('<H', yoff)
-    res += struct.pack('<H', width)
-    res += struct.pack('<H', height)
-    res += struct.pack('<H', 0)
-    res += struct.pack('<H', 0)
+    meta = codec, xoff, yoff, width, height, 0, 0
+    res = b''.join(struct.pack('<H', item) for item in meta)
+    # res = struct.pack('<H', codec)
+    # res += struct.pack('<H', xoff)
+    # res += struct.pack('<H', yoff)
+    # res += struct.pack('<H', width)
+    # res += struct.pack('<H', height)
+    # res += struct.pack('<H', 0)
+    # res += struct.pack('<H', 0)
     res += encode(width, height, data, codec if not force else force)
     if len(res) % 2 == 1:
         res += '\x00'
@@ -30,11 +34,13 @@ def untag(stream, force=None):
 def unobj(data):
     stream = io.BytesIO(data)
     stream = untag(stream, force='FOBJ')
-    codec = struct.unpack('<H', stream.read(2))[0]
-    stream.seek(4 ,1) # xoff + yoff
-    width = struct.unpack('<H', stream.read(2))[0]
-    height = struct.unpack('<H', stream.read(2))[0]
-    stream.seek(4, 1) # zeros
+    meta = (struct.unpack('<H', stream.read(2))[0] for _ in meta_struct)
+    codec, _, _, width, height, _, _ = meta
+    # codec = struct.unpack('<H', stream.read(2))[0]
+    # stream.seek(4 ,1) # xoff + yoff
+    # width = struct.unpack('<H', stream.read(2))[0]
+    # height = struct.unpack('<H', stream.read(2))[0]
+    # stream.seek(4, 1) # zeros
     data = stream.read()
     return width, height, decode(width, height, data), codec
 
@@ -56,7 +62,7 @@ def write_nut_file(header, numChars, chars, filename):
     header = struct.pack('<H', 2) + struct.pack('<H', numChars) + header[4:]
     header = mktag('AHDR', header)
 
-    nutFile = mktag('ANIM', header + ''.join(chars))
+    nutFile = mktag('ANIM', header + b''.join(chars))
 
     with open(filename, 'wb') as fontFile:
         fontFile.write(nutFile)
