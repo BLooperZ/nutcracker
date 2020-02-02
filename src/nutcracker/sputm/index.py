400d979726a85251b2ed92ac34d3a0fb1d6f5b25
BLooperZ <blooperz@users.noreply.github.com>
2020-02-02 22:02:21 +0200

    element tree poc

--- src/nutcracker/sputm/index.py
+++ src/nutcracker/sputm/index.py
@@ -2,8 +2,8 @@
 import io
 import os
 import struct
+from typing import Sequence, NamedTuple
 
-from functools import partial
 
 LEAF_CHUNKS = {
     'LOFF',
@@ -89,6 +89,26 @@ LEAF_CHUNKS = {
     'NOTE'
 }
 
+def findall(tag, root):
+    for c in root.children:
+        if c.tag == tag:
+            yield c
+
+def find(tag, root):
+    return next(findall(tag, root), None)
+
+def findpath(path, root):
+    path = os.path.normpath(path)
+    if not path or path == '.':
+        return root
+    dirname, basename = os.path.split(path)
+    return find(basename, findpath(dirname, root))
+
+class Element(NamedTuple):
+    tag: str
+    attribs: dict
+    children: Sequence[NamedTuple]
+
 def read_index(data, level=0, base_offset=0):
     chunks = sputm.print_chunks(sputm.read_chunks(data), level=level, base=base_offset)
     for idx, (hoff, (tag, chunk)) in enumerate(chunks):
@@ -100,11 +120,41 @@ def read_index(data, level=0, base_offset=0):
                 for rnum, off in zip(rnums, offs):
                     print(rnum, off)
 
-def map_chunks(data, level=0, base_offset=0):
-    chunks = sputm.print_chunks(sputm.read_chunks(data), level=level, base=base_offset)
-    for idx, (hoff, (tag, chunk)) in enumerate(chunks):
-        if tag not in LEAF_CHUNKS:
-            map_chunks(chunk, level=level + 1, base_offset=0)  # hoff + 8)
+def h(element, level=0):
+    attribs = ''.join(f' {key}="{value}"' for key, value in element.attribs.items())
+    indent = '    ' * level
+    closing = '' if element.children else ' /'
+    print(f'{indent}<{element.tag}{attribs}{closing}>')
+    if element.children:
+        for c in element.children:
+            h(c, level=level + 1)
+        print(f'{indent}</{element.tag}>')
+
+def map_chunks(data, base_offset=0):
+    chunks = sputm.read_chunks(data)
+    return [Element(
+        tag,
+        {'offset': hoff, 'size': len(chunk), 'absolute': base_offset + hoff},
+        [] if tag in LEAF_CHUNKS else map_chunks(chunk, base_offset=base_offset + hoff + 8)
+    ) for hoff, (tag, chunk) in chunks]
+
+def map_chunks_poc(data, base_offset=0):
+    try:
+        chunks = sputm.read_chunks(data)
+        return [Element(
+            tag,
+            {'offset': hoff, 'size': len(chunk), 'absolute': base_offset + hoff},
+            map_chunks(chunk, base_offset=base_offset + hoff + 8)
+        ) for hoff, (tag, chunk) in chunks]
+    except:
+        return []
+
+def create_maptree(data):
+    return map_chunks_poc(data)[0]
+
+def read_element(elem, stream):
+    stream.seek(elem.attribs['absolute'], io.SEEK_SET)
+    return stream.read(elem.attribs['size'])
 
 if __name__ == '__main__':
     import argparse
@@ -116,5 +166,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     with open(args.filename, 'rb') as res:
-        map_chunks(res.read())
-        print('==========')
+        root = create_maptree(res.read())
+        for lflf in findall('LFLF', root):
+            tree = findpath('RMIM/IM00', lflf)
+            # print(read_element(tree, res))
+            h(tree)
+
+        # print('==========')
