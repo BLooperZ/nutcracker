3cd06b3d1598540447895d04afd31d53dc3e1f04
BLooperZ <blooperz@users.noreply.github.com>
2021-01-20 23:03:20 +0200

    update smush schema

--- src/nutcracker/smush/schema.py
+++ src/nutcracker/smush/schema.py
@@ -5,12 +5,14 @@ RAWD = '____'  # Collect rest of chunk as raw data
 SCHEMA = {
     'ANIM': {'AHDR', 'FRME'},
     'AHDR': DATA,
-    'FRME': {'FTCH', 'IACT', 'XPAL', 'TEXT', 'STOR', 'FOBJ', 'NPAL'},
+    'FRME': {'FTCH', 'IACT', 'XPAL', 'TEXT', 'STOR', 'FOBJ', 'NPAL', 'TRES', 'PSAD'},
     'FTCH': DATA,
     'IACT': DATA,
     'XPAL': DATA,
     'TEXT': DATA,
     'STOR': DATA,
     'FOBJ': DATA,
-    'NPAL': DATA
+    'NPAL': DATA,
+    'TRES': DATA,
+    'PSAD': DATA,
 }
