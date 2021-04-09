30a4cf333b37e64c72c75d2bb325d6af5758bf60
BLooperZ <blooperz@users.noreply.github.com>
2021-04-09 07:03:14 +0300

    minor tweaks

--- src/nutcracker/kernel/align.py
+++ src/nutcracker/kernel/align.py
@@ -3,12 +3,19 @@ import deal
 from .buffer import BufferLike, splice
 
 
-@deal.raises(ValueError)
-@deal.reason(ValueError, lambda _: _.pad and set(_.pad) != {0})
+class NonZeroPaddingError(ValueError):
+    def __init__(self, pad: BufferLike) -> None:
+        super().__init__(
+            f'non-zero padding between chunks: {str(pad)}')
+        self.pad = pad
+
+
+@deal.raises(NonZeroPaddingError)
+@deal.reason(NonZeroPaddingError, lambda _: _.pad and set(_.pad) != {0})
 @deal.has()
 def assert_zero(pad: bytes) -> bytes:
     if pad and set(pad) != {0}:
-        raise ValueError(f'non-zero padding between chunks: {str(pad)}')
+        raise NonZeroPaddingError(pad)
     return pad
 
 
@@ -26,7 +33,9 @@ def calc_align(offset: int, align: int) -> int:
 @deal.pre(lambda _: 0 <= _.pos <= len(_.data))
 @deal.ensure(lambda _: _.pos <= _.result < _.pos + _.align)
 @deal.ensure(lambda _: _.result % _.align == 0)
-@deal.raises(ValueError)
+@deal.raises(NonZeroPaddingError)
+@deal.reason(NonZeroPaddingError, lambda _: set(_.data[_.pos:][:calc_align(_.pos, _.align)]) != {0})
+@deal.has()
 def align_read(data: BufferLike, pos: int, align: int = 1) -> int:
     """Align given read stream to next aligned position.
     Verify padding between chunks is zero.
@@ -34,7 +43,11 @@ def align_read(data: BufferLike, pos: int, align: int = 1) -> int:
     pad = assert_zero(splice(data, pos, calc_align(pos, align)))
     return pos + len(pad)
 
+
 @deal.pre(lambda _: _.align >= 1)
+@deal.ensure(lambda _: _.result.startswith(_.data))
+@deal.ensure(lambda _: len(_.result) == len(_.data) or set(_.result[len(_.data):]) == {0})
+@deal.ensure(lambda _: len(_.data) <= len(_.result) < len(_.data) + _.align)
 @deal.ensure(lambda _: len(_.result) % _.align == 0)
 @deal.pure
 def align_write(data: BufferLike, align: int = 1) -> bytes:
--- src/nutcracker/utils/funcutils.py
+++ src/nutcracker/utils/funcutils.py
@@ -6,7 +6,7 @@ T = TypeVar('T')
 
 def flatten(ls: Iterable[Iterable[T]]) -> Iterator[T]:
     # flatten(['ABC', 'DEF']) --> A B C D E F
-    "Flatten one level of nesting."
+    """Flatten one level of nesting."""
     return chain.from_iterable(ls)
 
 
